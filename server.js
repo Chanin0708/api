370d514cc69947458e4699906225b3c9e40a0b89
chanin <chanin.ph@intelligist.co.th>
2022-12-28 15:59:24 +0700

    update

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ let app = express();
 let bodyParser = require('body-parser');
 let mysql = require('mysql');
 const cors = require('cors');
-const multer = require('multer'); 
+const multer = require('multer');
 const upload = multer({ dest: 'uploads/' });
 const MongoClient = require('mongodb').MongoClient;
-
+const mongodb = require('mongodb');
+const mongoUrl = 'mongodb://dev01:zjkoC%5D6p@192.168.1.122:27017/?authMechanism=DEFAULT&authSource=admin';
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -18,55 +19,28 @@ app.use(cors({ origin: '*' }));
 
 // homepage route
 app.get('/', (req, res) => {
-    return res.send({ 
-        error: false, 
-        message: 'Welcome to RESTful CRUD API with NodeJS, Express, MYSQL',
-    })
+  return res.send({
+    message: 'Welcome to RESTful CRUD API with NodeJS, Express, Mongo',
+  })
 })
 
-// connection to mysql database
-// let dbConnection = mysql.createConnection({
-//     host: 'localhost',
-//     port: '3306',
-//     user: 'root',
-//     password: 'root',
-//     database: 'nodejs_api'
-// })
-// dbConnection.connect();
-
-MongoClient.connect('mongodb://dev01:zjkoC%5D6p@192.168.1.122:27017/?authMechanism=DEFAULT&authSource=admin', (err, client) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  const db = client.db('mydatabase');
-});
-
 // retrieve all fruits 
-MongoClient.connect('mongodb://dev01:zjkoC%5D6p@192.168.1.122:27017/?authMechanism=DEFAULT&authSource=admin', (err, client) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  const db = client.db('admin');
-
-  // Define the file upload endpoint
-  app.get('/getdatafruit', (req, res) => {
-    db.collection('fruits').find().toArray((err, results) => {
+app.get('/getdatafruit', (req, res) => {
+  mongodb.MongoClient.connect(mongoUrl, (err, client) => {
+    if (err) throw err;
+    const db = client.db('admin');
+    const collection = db.collection('fruits');
+    collection.find().toArray((err, results) => {
       if (err) {
         console.error(err);
         return res.sendStatus(500);
       }
-  
       let message = "";
       if (results.length === 0) {
         message = "No documents found in the collection";
       } else {
         message = "Successfully retrieved all documents";
       }
-  
-
-  
       return res.send({ data: results, message: message });
     });
   });
@@ -74,41 +48,17 @@ MongoClient.connect('mongodb://dev01:zjkoC%5D6p@192.168.1.122:27017/?authMechani
 
 
 // add a new fruit
-// app.post('/addfruit', (req, res) => {
-//     let name = req.body.name;
-//     let author = req.body.author;
-
-//     // validation
-//     if (!name || !author) {
-//         return res.status(400).send({ message: "Please provide fruit name and author."});
-//     } else {
-//         dbConnection.query('INSERT INTO fruits (name, author) VALUES(?, ?)', [name, author], (error, results, fields) => {
-//             if (error) throw error;
-//             return res.send({message: "fruit successfully added"})
-//         })
-//     }
-// });
-MongoClient.connect('mongodb://dev01:zjkoC%5D6p@192.168.1.122:27017/?authMechanism=DEFAULT&authSource=admin', (err, client) => {
-  if (err) {
-    console.error(err);
-    return;
+app.post('/upload', upload.single('photo'), (req, res) => {
+  let name = req.body.name;
+  let photo = req.file;
+  if (!name || !photo) {
+    return res.status(400).send({ message: 'Please provide a value, and photo.' });
   }
-  const db = client.db('admin');
-
-  // Define the file upload endpoint
-  app.post('/upload', upload.single('photo'), (req, res) => {
-    // extract the value and photo from the request body
-    let name = req.body.name;
-    let author = req.body.author;
-    let photo = req.file;
-  
-    // validate the input
-    if (!name || !author || !photo) {
-      return res.status(400).send({ message: 'Please provide a value, author, and photo.' });
-    }
-  
-    // insert the value and photo into the database
-    db.collection('fruits').insertOne({ name: name, author: author, photo: photo.path }, (err, result) => {
+  mongodb.MongoClient.connect(mongoUrl, (err, client) => {
+    if (err) throw err;
+    const db = client.db('admin');
+    const collection = db.collection('fruits');
+    collection.insertOne({ name: name, photo: photo.path }, (err, result) => {
       if (err) {
         console.error(err);
         return res.sendStatus(500);
@@ -118,98 +68,93 @@ MongoClient.connect('mongodb://dev01:zjkoC%5D6p@192.168.1.122:27017/?authMechani
   });
 });
 
-
-// app.post('/upload', upload.single('photo'), (req, res) => {
-//     // extract the value and photo from the request body
-//     let name = req.body.name;
-//     let author = req.body.author;
-//     let photo = req.file;
-  
-//     // validate the input
-//     if (!name || !author ||  !photo) {
-//       return res.status(400).send({ message: 'Please provide a value and a photo.' });
-//     }
-  
-//     // insert the value and photo into the database
-//     db.collection('fruits').insertOne({ name: name, author: author, photo: photo.path }, (err, result) => {
-//         if (err) {
-//           console.error(err);
-//           return res.sendStatus(500);
-//         }
-//         return res.send({ message: 'Value and photo successfully added.' });
-//       });
-//   });
-
-// retrieve fruit by name 
 app.get('/getdatafruit/:name', (req, res) => {
-    let name = req.params.name;
+  let name = req.params.name;
+  if (!name) {
+    return res.status(400).send({ message: 'Please provide fruit name' });
+  } else {
+    mongodb.MongoClient.connect(mongoUrl, (err, client) => {
+      if (err) throw err;
+      const db = client.db('admin');
+      const collection = db.collection('fruits');
+      collection.findOne({ name: name }, (err, result) => {
+        if (err) throw err;
+        let message = '';
+        if (!result) {
+          message = 'fruit not found';
+        } else {
+          message = 'Successfully retrieved fruit data';
+        }
+        return res.send({ data: result, message: message });
+      });
+    });
+  }
+});
 
-    if (!name) {
-        return res.status(400).send({ message: "Please provide fruit name"});
-    } else {
-        dbConnection.query("SELECT * FROM fruits WHERE name = ?", name, (error, results, fields) => {
-            if (error) throw error;
-
-            let message = "";
-            if (results === undefined || results.length == 0) {
-                message = "fruit not found";
-            } else {
-                message = "Successfully retrieved fruit data";
-            }
-            return res.send({data: results[0], message: message })
-        })
-    }
-})
-
-// update fruits with id 
-app.put('/updatefruit', (req, res) => {
-    let id = req.body.id;
-    let name = req.body.name;
-    let author = req.body.author;
-
-    // validation
-    if (!id || !name || !author) {
-        return res.status(400).send({ message: 'Please provide fruit id, name and author'});
-    } else {
-        dbConnection.query('UPDATE fruits SET name = ?, author = ? WHERE id = ?', [name, author, id], (error, results, fields) => {
-            if (error) throw error;
-
-            let message = "";
-            if (results.changedRows === 0) {
-                message = "fruit not found or data are same";
-            } else {
-                message = "fruit successfully updated";
-            }
-
-            return res.send({ error: false, data: results, message: message })
-        })
-    }
-})
-
-// delete fruit by id
+// delete fruit by name
 app.delete('/deletefruit', (req, res) => {
-    let id = req.body.id;
+  let name = req.body.name;
+  if (!name) {
+    return res.status(400).send({ error: true, message: "Please provide fruit name" });
+  }
+  mongodb.MongoClient.connect(mongoUrl, (err, client) => {
+    if (err) throw err;
+    const db = client.db('admin');
+    const collection = db.collection('fruits');
+    collection.deleteOne({ name: name }, (err, result) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send({ message: 'An error occurred while deleting the fruit' });
+      }
 
-    if (!id) {
-        return res.status(400).send({ error: true, message: "Please provide fruit id"});
-    } else {
-        dbConnection.query('DELETE FROM fruits WHERE id = ?', [id], (error, results, fields) => {
-            if (error) throw error;
+      if (result.deletedCount === 0) {
+        return res.status(404).send({ message: 'Fruit not found' });
+      }
 
-            let message = "";
-            if (results.affectedRows === 0) {
-                message = "fruit not found";
-            } else {
-                message = "fruit successfully deleted";
-            }
+      return res.send({ message: 'Fruit successfully deleted' });
+    });
+  });
+});
 
-            return res.send({ error: false, data: results, message: message })
-        })
-    }
-})
+app.put('/updatefruit', (req, res) => {
+  let id = req.body.id;
+  let name = req.body.name;
+  let photo = req.file;
+
+  // Validation
+  if (!id || !name || !photo) {
+    return res.status(400).send({ message: 'Please provide fruit id, name, and photo' });
+  }
+  mongodb.MongoClient.connect(mongoUrl, (err, client) => {
+    if (err) throw err;
+    const db = client.db('admin');
+    const collection = db.collection('fruits');
+    const update = {
+      $set: {
+        name: name,
+        photo: photo.path
+      }
+    };
+    const options = {
+      returnOriginal: false
+    };
+    collection.findOneAndUpdate({ _id: ObjectId(id) }, update, options, (err, result) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send({ message: 'An error occurred while updating the fruit' });
+      }
+
+      if (result.value === null) {
+        return res.status(404).send({ message: 'Fruit not found' });
+      }
+
+      return res.send({ message: 'Fruit successfully updated' });
+    });
+  });
+});
+
 
 app.listen(3000, () => {
-    console.log('Node App is running on port 3000');
+  console.log('Node App is running on port 3000');
 })
-
 module.exports = app;
